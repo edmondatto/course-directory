b7b0daab2e42a53d14c77a1ed1e19b734239c547
Edmond Atto <edmond091@gmail.com>
2017-10-31 16:19:32 +0300

    [CHORE] Refactor code to remove repititive course rendering functions

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -4,9 +4,13 @@ import {
   Route,
   Redirect,
 } from 'react-router-dom';
-import HTML from "./courses/HTML";
-import CSS from "./courses/CSS";
-import JavaScript from "./courses/JavaScript";
+import CourseContainer from "./courses/CourseContainer";
+import {
+  HTMLCourses,
+  CSSCourses,
+  JSCourses
+} from '../data/courses'
+
 
 const Courses = ({match}) => (
   <div className="main-content courses">
@@ -20,9 +24,18 @@ const Courses = ({match}) => (
     </div>
 
     <Route exact path={match.path} render={() => <Redirect to={`${match.path}/html`}/>}/>
-    <Route path={`${match.path}/html`} component={HTML}/>
-    <Route path={`${match.path}/css`} component={CSS}/>
-    <Route path={`${match.path}/javascript`} component={JavaScript}/>
+    <Route
+      path={`${match.path}/html`}
+      render={() => <CourseContainer data={HTMLCourses}/>}
+    />
+    <Route
+      path={`${match.path}/css`}
+      render={() => <CourseContainer data={CSSCourses}/>}
+    />
+    <Route
+      path={`${match.path}/javascript`}
+      render={() => <CourseContainer data={JSCourses}/>}
+    />
   </div>
 );
 
--- src/components/courses/CourseContainer.js
+++ src/components/courses/CourseContainer.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import Course from './Course';
-import { JSCourses } from '../../data/courses';
+import PropTypes from 'prop-types';
 
-const JavaScript = (props) => {
-  let courses = JSCourses.map((course) => {
+
+const CourseContainer = (props) => {
+  let courses = props.data.map((course) => {
     return <Course title={course.title}
                    desc={course.description}
                    img={course.img_src}
                    key={course.id} />
-  }); 
+  });
   return (
     <div>
       <ul>
-        {courses}    
+        {courses}
       </ul>
     </div>
   );
-}
+};
 
-export default JavaScript;
+CourseContainer.propTypes = {
+  data: PropTypes.array.isRequired,
+};
+
+export default CourseContainer;
